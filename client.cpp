78d16e902bdcce74f3bc4e5212f7fdaf0961f486
Ayan Nair <ayannair23@gmail.com>
2025-06-28 00:15:30 -0700

    added intro server code

--- client.cpp
+++ client.cpp
@@ -8,3 +8,60 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+static void do_something(int connfd) {
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1); // can replace read with recv
+    if (n < 0) {
+        msg("read() error");
+        return;
+    }
+    fprintf(stderr, "client says: %s\n", rbuf);
+
+    char wbuf[] = "world";
+    write(connfd, wbuf, strlen(wbuf)); // can replace write with send
+}
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0); // obtain socket handle
+
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)); // set socket options ALWAYS DO THIS
+
+    // bind to address 1234
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);        // port
+    addr.sin_addr.s_addr = htonl(0);    // wildcard IP 0.0.0.0
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) { die("bind()"); }
+
+    // listen: socket actually created here
+    rv = listen(fd, SOMAXCONN);
+    if (rv) { die("listen()"); }
+
+    while (true) {
+        // accept
+        struct sockaddr_in client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+        if (connfd < 0) {
+            continue;   // error
+        }
+
+        do_something(connfd);
+        close(connfd);
+    }
+
+    return 0;
+
+}
